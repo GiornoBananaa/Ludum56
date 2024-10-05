b35109eb83a3f007628acd0ab86ecd1627b3323d
AFK-Shark <graffin.may@yandex.ru>
2024-10-05 23:58:43 +0300

    refactor: PlayerControl 0.1

--- Assets/_Source/Player/Player.cs
+++ Assets/_Source/Player/Player.cs
@@ -7,4 +7,6 @@ public class Player : MonoBehaviour
     public int attackPower = 10;
     public int hp = 1;
     public float attackRadius = 5f;
+
+
 }
--- Assets/_Source/Player/PlayerControl.cs
+++ Assets/_Source/Player/PlayerControl.cs
@@ -8,8 +8,12 @@ public class PlayerControl : MonoBehaviour
     [SerializeField] private float _moveSpeed;
 
     public SpriteRenderer playerSprite;
+    public BoxCollider2D playerCollider;
 
     private Vector2 _moveDirection;
+
+    private bool isSkillAvailable = true;
+    private bool isAttackAvailable = true;
 
     public void OnMove(InputAction.CallbackContext context)
     {
@@ -19,20 +23,47 @@ public class PlayerControl : MonoBehaviour
 
     public void OnAttack(InputAction.CallbackContext context)
     {
-        Debug.Log("Attack");
+        if (isAttackAvailable && isSkillAvailable)
+        {
+            isAttackAvailable = false;
+            Debug.Log("Attack");
+            Invoke(nameof(EnableAttack), 1.25f);
+        }
     }
 
     public void OnSkills(InputAction.CallbackContext context)
     {
-        playerSprite.enabled = false;
-        Invoke(nameof(ShowPlayerSprite), 3f);
+        if (isSkillAvailable)
+        {
+            isSkillAvailable = false;
+            isAttackAvailable = false;
+            playerSprite.enabled = false;
+            playerCollider.enabled = false;
 
-        Debug.Log("Ну типо DOWN, ок?");
+            _moveSpeed = _moveSpeed + 5;
+
+            Invoke(nameof(ShowPlayerSprite), 3f);
+            Invoke(nameof(EnableSkills), 2f);
+            Invoke(nameof(EnableAttack), 3f);
+            Debug.Log("Ну типо DOWN, ок?");
+        }
     }
 
     private void ShowPlayerSprite()
     {
+        _moveSpeed = _moveSpeed - 5;
         playerSprite.enabled = true;
+        playerCollider.enabled = true;
+    }
+
+    private void EnableAttack()
+    {
+        isAttackAvailable = true;
+    }
+
+    private void EnableSkills()
+    {
+        isSkillAvailable = true;
     }
 
     private void Move(Vector3 direction)
