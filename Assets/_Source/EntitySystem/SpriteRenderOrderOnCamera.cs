debbeaa8fc701f14bf6f7b2e398fcdf1c91442bc
GiornoBananaa <105521406+GiornoBananaa@users.noreply.github.com>
2024-10-07 01:57:10 +0300

    Fixed attack effect sorting order

--- Assets/_Source/EntitySystem/SpriteRenderOrderOnCamera.cs
+++ Assets/_Source/EntitySystem/SpriteRenderOrderOnCamera.cs
@@ -8,6 +8,7 @@ namespace EntitySystem
         private const int MAX_LAYER = 200;
     
         [SerializeField] private SpriteRenderer _spriteRenderer;
+        [SerializeField] private SpriteRenderer[] _spritesOnTop;
     
         private Camera _camera;
 
@@ -25,6 +26,10 @@ namespace EntitySystem
         {
             Vector2 screenPosition = _camera.WorldToScreenPoint(transform.position);
             _spriteRenderer.sortingOrder = (int)Mathf.Lerp(MIN_LAYER,MAX_LAYER, (_camera.pixelHeight - screenPosition.y) / _camera.pixelHeight);
+            for (int i = 0; i < _spritesOnTop.Length; i++)
+            {
+                _spritesOnTop[i].sortingOrder = _spriteRenderer.sortingOrder + i + 1;
+            }
         }
     }
 }
