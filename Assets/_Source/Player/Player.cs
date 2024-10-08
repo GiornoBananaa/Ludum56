e781b78159299d883c72c54b0ce00010cdc34a24
AFK-Shark <159244757+AFK-Shark@users.noreply.github.com>
2024-10-08 16:20:08 +0300

    fix: VIsual HP

--- Assets/_Source/Player/Player.cs
+++ Assets/_Source/Player/Player.cs
@@ -3,35 +3,50 @@ using System.Collections;
 using System.Collections.Generic;
 using EntitySystem.CombatSystem;
 using UnityEngine;
+using UnityEngine.UI; // Add this namespace for UI components
 
 public class Player : MonoBehaviour, IDamageable
 {
     [field: SerializeField] protected bool kill;
-    
+
     public int attackPower = 1;
     public int passiveAttack = 0;
     public int AttackJerk = 1;
 
     public int hp = 1;
     public float attackRadius = 5f;
-    
+
     private bool IsDead;
     public Action OnDeath;
 
+    // Add references to the HP images
+    public Image hpImage1;
+    public Image hpImage2;
+
     private void FixedUpdate()
     {
-        if(kill && hp > 0)
+        if (kill && hp > 0)
             TakeDamage(100);
     }
 
     public void TakeDamage(int damage)
     {
         hp -= damage;
-        
-        if(!IsDead && hp <= 0)
+
+        if (!IsDead && hp <= 0)
         {
             IsDead = true;
             OnDeath?.Invoke();
+        }
+
+        // Update the HP images based on the player's HP
+        if (hp < 1)
+        {
+            hpImage1.enabled = false; // Hide the first image
+        }
+        else
+        {
+            hpImage1.enabled = true; // Show the first image
         }
     }
 }
